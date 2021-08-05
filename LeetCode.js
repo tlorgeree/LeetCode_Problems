6308f8644d1190b2add86ec0a8e4bfbd0de85f1a
Tim Lorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-08-05 12:15:03 -0400

    Merge Sorted Lists

--- LeetCode.js
+++ LeetCode.js
@@ -252,3 +252,44 @@ The solution set must not contain duplicate subsets. Return the solution in any 
     
     return output;
 };
+
+/*Merge two sorted linked lists and return it as a sorted list. 
+The list should be made by splicing together the nodes of the first two lists*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+ var mergeTwoLists = function(l1, l2) {
+    if(l1 === null && l2 === null) return null;
+    if(l1 === null && l2 !== null) return l2;
+    if(l1 !== null && l2 === null) return l1;
+    let output = new ListNode();
+    let temp = output;
+    while (l1 && l2)
+        {
+            if (l1.val < l2.val)
+                {
+                    temp.next = l1;
+                    l1 = l1.next;
+                }
+            else
+                {
+                    temp.next = l2;
+                    l2 = l2.next;
+                }
+            temp = temp.next;
+        }
+    temp.next = l1 || l2;
+    return output.next;
+
+};
