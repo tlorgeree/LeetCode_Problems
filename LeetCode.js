0400bf195e71ab91c13df9ed7721c6ba1e877fc9
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-08-05 22:03:27 -0400

    Needle in Hystack problem

--- LeetCode.js
+++ LeetCode.js
@@ -319,3 +319,19 @@ array in-place with O(1) extra memory.*? */
     console.log(nums);
     return i + 1;
 };
+
+/*Implement strStr(). Return the index of the first 
+occurrence of needle in haystack, or -1 if needle is not part of haystack.
+Clarification: What should we return when needle is an empty 
+string? This is a great question to ask during an interview.
+For the purpose of this problem, we will return 0 when needle 
+is an empty string. This is consistent to C's strstr() and Java's indexOf().*/
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle); //"Hey, I've seen this befrore"
+};
