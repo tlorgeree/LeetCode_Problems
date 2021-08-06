8f2c0d68a961082da6615e320156faf879c5a4ae
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-08-05 20:25:37 -0400

    updates with stupid problem

--- LeetCode.js
+++ LeetCode.js
@@ -293,3 +293,29 @@ The list should be made by splicing together the nodes of the first two lists*/
     return output.next;
 
 };
+
+/*Given an integer array nums sorted in non-decreasing order, remove the duplicates 
+in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+Since it is impossible to change the length of the array in some languages, 
+you must instead have the result be placed in the first part of the array nums. More formally,
+if there are k elements after removing the duplicates, then the first k elements of nums 
+should hold the final result. It does not matter what you leave beyond the first k elements.
+Return k after placing the final result in the first k slots of nums.
+Do not allocate extra space for another array. You must do this by modifying the input 
+array in-place with O(1) extra memory.*? */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    if (nums.length == 0) return 0;
+    let i = 0;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    console.log(nums);
+    return i + 1;
+};
