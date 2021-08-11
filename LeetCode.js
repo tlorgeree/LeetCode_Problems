f763d6ac0c7ff84552b040bfbba7ea5c08229361
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-08-10 22:17:26 -0400

    Plus one problem

--- LeetCode.js
+++ LeetCode.js
@@ -461,3 +461,33 @@ A word is a maximal substring consisting of non-space characters only.*/
     const s_arr = s.trim().split(" ");
     return s_arr[s_arr.length-1].length;
 };
+
+/*Given a non-empty array of decimal digits representing 
+a non-negative integer, increment one to the integer.
+The digits are stored such that the most significant digit is at the
+head of the list, and each element in the array contains a single digit.
+You may assume the integer does not contain any leading zero, 
+except the number 0 itself.*/
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+ var plusOne = function(digits) {
+    for(let i = (digits.length - 1); i >=0; i--)
+        {
+            if (!(digits[i] == 9))
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+                {
+                    digits[i] = 0;              
+                    if (i == 0) 
+                    {
+                        digits.splice(0,0,1);
+                        return digits;
+                    }
+                }
+        }
+};
